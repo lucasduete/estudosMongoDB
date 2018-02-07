229ced769219d1070a6309bb5e70aa0da772b0d9
Lucas Duete <lm.duete@gmail.com>
2018-02-07 20:46:03 -0300

    Melhorando Camada de Apresentação.

--- src/main/java/io/github/lucasduete/App.java
+++ src/main/java/io/github/lucasduete/App.java
@@ -4,18 +4,89 @@ import io.github.lucasduete.controller.PublicacaoController;
 import io.github.lucasduete.model.Publicacao;
 import org.bson.types.ObjectId;
 
+import java.util.Scanner;
+
 
 public class App {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        //Inserir Publicacoes Padroes Para Agilisar
         inserirPubs();
 
+        boolean continuar = true;
+
+        do {
+
+            System.out.println("Digite o que deseja fazer: ");
+            System.out.println("1 - Cadastrar Publicacao;");
+            System.out.println("2 - Buscar Publicacao;");
+            System.out.println("0 - Sair;");
+
+            int aux = scanner.nextInt();
+
+            switch (aux) {
+                case 1:
+                    cadastrarPub();
+                    break;
+                case 2:
+                    buscarPub();
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("OPCAO INVALIDA");
+            }
+
+            System.out.println("Deseja Continuar?");
+            System.out.println("1 Para Sim");
+            System.out.println("Digite Outra Tecla para Sair");
+
+            if (aux == 1)
+                continuar = true;
+            else
+                continuar = false;
+
+        } while (continuar);
+
+    }
+
+    public static void cadastrarPub() {
+
         PublicacaoController publicacaoController = new PublicacaoController();
-        System.out.println(publicacaoController.buscarPublicacao("Musicas"));
+
+        System.out.println("Digite o Titulo:");
+        String titulo = scanner.next();
+
+        System.out.println("Digite o Autor:");
+        String autor = scanner.next();
+
+        System.out.println("Digite o Conteudo da Publicacao:");
+        String conteudo = scanner.next();
+
+        Publicacao pub = new Publicacao(new ObjectId(), titulo, autor, conteudo);
+        publicacaoController.salvarPublicacao(pub);
+    }
+
+    public static void buscarPub() {
+
+        PublicacaoController publicacaoController = new PublicacaoController();
+
+        System.out.println("Digite um Texto para Filtar as Publicacoes");
+
+        String query = scanner.next();
+
+        publicacaoController.buscarPublicacao(query)
+                .forEach( publicacao -> {
+                    System.out.println(publicacao);
+                }
+        );
+
     }
 
     public static void inserirPubs() {
+
         Publicacao pub = null;
         PublicacaoController publicacaoController = new PublicacaoController();
 
@@ -34,4 +105,5 @@ public class App {
         pub = new Publicacao(new ObjectId(), "Linux supera Windows", "Lucas", "Pela prim vez os usuarios de linux em geral ultrapassam os de windows");
         publicacaoController.salvarPublicacao(pub);
     }
+
 }
