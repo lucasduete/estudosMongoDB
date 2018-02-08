8894c18377c6cfe436ba78271493b32216a5d26c
Lucas Duete <lm.duete@gmail.com>
2018-02-07 21:13:28 -0300

    Finalizando Projeto.

--- src/main/java/io/github/lucasduete/App.java
+++ src/main/java/io/github/lucasduete/App.java
@@ -9,89 +9,28 @@ import java.util.Scanner;
 
 public class App {
 
-    private static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
 
-        //Inserir Publicacoes Padroes Para Agilisar
-        inserirPubs();
-
-        boolean continuar = true;
-
-        do {
-
-            System.out.println("Digite o que deseja fazer: ");
-            System.out.println("1 - Cadastrar Publicacao;");
-            System.out.println("2 - Buscar Publicacao;");
-            System.out.println("0 - Sair;");
-
-            int aux = scanner.nextInt();
-
-            switch (aux) {
-                case 1:
-                    cadastrarPub();
-                    break;
-                case 2:
-                    buscarPub();
-                    break;
-                case 0:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("OPCAO INVALIDA");
-                    break;
-            }
-
-            System.out.println("Deseja Continuar?");
-            System.out.println("1 Para Sim");
-            System.out.println("Digite Outra Tecla para Sair");
-
-            if (aux == 1)
-                continuar = true;
-            else
-                continuar = false;
-
-        } while (continuar);
-
-    }
-
-    public static void cadastrarPub() {
-
         PublicacaoController publicacaoController = new PublicacaoController();
 
-        System.out.println("Digite o Titulo:");
-        String titulo = scanner.next();
+        //Inserir Publicacoes Padroes Para Agilizar
+        inserirPubs(publicacaoController);
 
-        System.out.println("Digite o Autor:");
-        String autor = scanner.next();
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Digite um Texto para Filtar as Publicacoes");
+//        String query = scanner.next();
 
-        System.out.println("Digite o Conteudo da Publicacao:");
-        String conteudo = scanner.next();
-
-        Publicacao pub = new Publicacao(new ObjectId(), titulo, autor, conteudo);
-        publicacaoController.salvarPublicacao(pub);
-    }
-
-    public static void buscarPub() {
-
-        PublicacaoController publicacaoController = new PublicacaoController();
-
-        System.out.println("Digite um Texto para Filtar as Publicacoes");
-
-        String query = scanner.next();
-
-        publicacaoController.buscarPublicacao(query)
+        publicacaoController.buscarPublicacao("Musicas")
                 .forEach( publicacao -> {
-                    System.out.println(publicacao);
-                }
-        );
-
+                            System.out.println(publicacao);
+                        }
+                );
     }
 
-    public static void inserirPubs() {
+    public static void inserirPubs(PublicacaoController publicacaoController) {
 
         Publicacao pub = null;
-        PublicacaoController publicacaoController = new PublicacaoController();
 
         pub = new Publicacao(new ObjectId(), "Livros Academicos Ultrapassados", "Lucas", "Os Livros academicos estao cada vez mais ultrapassados");
         publicacaoController.salvarPublicacao(pub);
